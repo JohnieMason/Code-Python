92ff5ea6f62b0813bb41c214078cec35576d0150
Johnie Mason <johniemason@students.uonbi.ac.ke>
2025-02-26 05:58:29 +0300

    Boom

---
+++ Code_python/intro.py
@@ -0,0 +1,15 @@
+#Data types
+int: (integers)
+float: (decimals)
+str: (string e.g "Hellow World")
+bool: (boolean)
+
+# Variables....just like containers that store data
+age = 25 # Integer
+height = 6.2 #decimal
+name = "Mason" #string, 
+is_graduate = True #boolean
+
+#printing outputs
+print("name", name)
+
